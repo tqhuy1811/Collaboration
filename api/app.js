b8f3b33db91f2150a68955c1511c473855ec2d7e
Tin Nguyen <nttin.tn20.cvn@gmail.com>
2020-04-08 16:18:13 +0700

    miidleware

--- api/app.js
+++ api/app.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+app.set("trust proxy", true);
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const requestTime = (req, res, next) => {
+  req.startTime = Date.now();
+  next();
+};
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+const responseTime = (req, res, next) => {
+  res.endTime = Date.now();
+  next();
+};
+
+const clientIP = (req, res, next) => {
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log(`Requested from: ${ip}`);
+  next();
+};
+
+app.use([requestTime, responseTime, clientIP]);
+
+app.get("/", (req, res) => {
+  const timeDuration = res.endTime - req.startTime;
+  console.log(timeDuration);
+  var responseText = "Hello World!<br>";
+  responseText += `<small>Request finished in: ${timeDuration}.</small> </br>`;
+  res.send(responseText);
+});
+
+app.listen(3000, () =>
+  console.log(`Example app listening at http://localhost:${3000}`)
 );
